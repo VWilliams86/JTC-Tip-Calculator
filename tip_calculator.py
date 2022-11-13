d1d62718dc6f63a7059c5d18258bcf6c03dc4e08
Valerie Trimble <valerie.trimble@gmail.com>
2022-11-13 17:45:00 -0600

    Updated Tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,54 +1,45 @@
-#Ask user what the total amount of the bill was to calulate tax. Note: needs to be in a function.
+#Ask the user what the total amount of the bill was to calculate tax. Note: needs to be in a function.
 def calculator():
     print("Hello, Welcome to The Handy Dandy Tip Calculator!!")
-#Ask user what the total amount of the bill was to calulate tax. 
-    bill = float(input("What was the total bill, without tax? $"))
-#Ask the user how much they would like to tip the server?
-    tip = float(input("What percentage tip would you like to give your friendly server? Do not enter % sign."))
-#Ask the user how many people will be need paying the total cost of the tab. 
-    number_of_people = int(input("How many people will be paying?"))
-#calculate sales tax and print 
+#Ask the user what the total amount of the bill was to calculate tax. 
+    try:
+        bill = float(input("What was the total bill, without tax? $"))
+    #Ask the user how much they would like to tip the server?
+        tip = float(input("What percentage tip would you like to give your friendly server? Do not enter % sign."))
+    #Ask the user how many people will be  paying the total cost of the tab. 
+        number_of_people = int(input("How many people will be paying?"))
+    except ValueError:
+        print('Oops! Please try again!')
+        calculator()
+    #Calculate sales tax and print 
     sales_tax = bill*.10
     print('Sales tax is $', \
     format (sales_tax,'.2f'))
-#calculate the bill  and sales tax, print.
+#Calculate the bill  and sales tax, print.
     total_price_with_tax= bill + sales_tax
     print('Your total bill with tax before tip is $',\
         format (total_price_with_tax,'.2f'))
-#calculate the amount tip total, print. Calualte the amount per person.
+#Calculate the amount tip total, print. Calculate the amount per person.
     tip_amount_per_person = "%.2f" % float(tip / 100 * bill) #* total_price) / number_of_people)  
     print(f"Tip amount: ${tip_amount_per_person}")
     tip_due = "%.2f" % float(((tip / 100 * bill) + total_price_with_tax )/ number_of_people)
     print (f'Total amount due per person!!!: ${tip_due}')
-    return tip_due, number_of_people
+    
 
-#Ask if customer would like to add aditional tip to check, continue to ask until they say no. 
+#Ask if the customer would like to add anaditional tip to check, and continue to ask until they say no. 
 def additional_tip_request():
-    tip_due , number_of_people= calculator()
-    additional_tip = bool(input("Would you like to give this server an additional tip? Type yes or no:"))
-    while True:
-        
-        if additional_tip == "yes":
-            extra_tip = float(input('How much would you like to add?:$'))
-        new_bill = (tip_due + extra_tip)
-        print (f'Total amount due per person!!!: ${new_bill}')
-        additional_tip = bool(input("Would you like to give this server an additional tip? Type yes or no:"))
-    
-    
-        if additional_tip == "no":
-            print('Thank you and Have a wonderful day!')
-        else :
-            additional_tip != "yes","no"
-        print('Please enter yes or no only!')
-        break
-        return  additional_tip_request()
+    additional_tip = input("Would you like to calculate a new bill? Type yes or no:")
+    while  additional_tip == "yes":
+        calculator()
+        additional_tip = input("We are inside the while loop. Would you like to calculate a new bill? Type yes or no:")
 
-#I am having an issue with the addition tip request, 
+ 
+    print('Thank you and have a wonderful day!')
+      
 
 
-
-additional_tip_request()       
 calculator()
+additional_tip_request()
 
 
 
